11e2d514997d3c9dce01c1497b3c5672ad11c7a1
gryf <gryf73@gmail.com>
2019-05-04 11:23:58 -0600

    Added configurable headings level
    Currently, there was no way for changing level of the heading, from
    which html would be generated. In this patch, there is added an ability
    to define it in jekyll _config.yml file for site.
    
    It's simple as adding following entries into _config.yml:
    
    rst:
      initial_header_level: 2
    
    If not defined, level 2 will be used as default.

--- converter.rb
+++ converter.rb
@@ -18,7 +18,17 @@ module Jekyll
     def convert(content)
       dirname = "#{File.expand_path(File.dirname(__FILE__))}"
       RbST.executables = {:html => dirname + "/rst2html5.py"}
-      rst2htmlcontent = RbST.new(content).to_html(:initial_header_level => 2)
+      level = 2
+
+      conf = Jekyll.configuration({})
+      if conf.has_key?('rst')
+        level = conf['rst']['initial_header_level']
+        if not level.is_a?(Integer)
+          level = 2
+        end
+      end
+
+      rst2htmlcontent = RbST.new(content).to_html(:initial_header_level => level)
       document = Nokogiri::HTML(rst2htmlcontent)
       content = document.css('body').inner_html
     end
